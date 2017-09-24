3bbdcb2c516aead7e14bf0938bf72ecfdfdd5ced
Mostyn Griffith (NewMAC) <mostyngriffith@mostyns-mbp-3.risd.edu>
2017-09-24 13:48:49 -0400

    FIRST VERSION

---
+++ js/script.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+
+  //MAIN DIVS
+    $(".cover").on("click", function(){
+      $(this).toggleClass("clicked");
+    });
+
+    $(".title").on("click", function(){
+      $(this).toggleClass("nicked")
+      $(".cover.clicked").toggleClass("nicked")
+
+      $(".infoButton").toggleClass("clicked");
+      $(".info").toggleClass("clicked");
+
+    });
+
+    //slide toggle for project content
+
+    $(".evan-roth").click(function(){
+        // $(this).slideToggle()
+        $(".content-evan-roth").toggle();
+    });
+
+
+    $(".epigenia").click(function(){
+        // $(this).slideToggle()
+        $(".content-epigenia").toggle();
+    });
+
+    $(".streetlight").click(function(){
+        // $(this).slideToggle()
+        $(".content-streetlight").toggle();
+    });
+
+    $(".edes").click(function(){
+        // $(this).slideToggle()
+        $(".content-edes").toggle();
+    });
+
+    $(".manzanar").click(function(){
+        // $(this).slideToggle()
+        $(".content-manzanar").toggle();
+    });
+
+    $(".raid").click(function(){
+        // $(this).slideToggle()
+        $(".content-raid").toggle();
+    });
+
+    $(".posters").click(function(){
+        // $(this).slideToggle()
+        $(".content-posters").toggle();
+    });
+
+    $(".cornell-dive").click(function(){
+        // $(this).slideToggle()
+        $(".content-cornell-dive").toggle();
+    });
+
+
+    
+let lazy = lazyload();
+
+    $("img.lazyload").lazyload();
+
+
+
+
+
+
+});
+
+
+
+
+
+
