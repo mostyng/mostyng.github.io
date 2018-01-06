770cc9e923c377b60ef378fa02052819e61f8e84
Mostyn Griffith (NewMAC) <mostyngriffith@mostyns-mbp-3.risd.edu>
2018-01-06 17:43:45 -0500

    v.04

--- js/script.js
+++ js/script.js
@@ -1,82 +1,80 @@
 $(document).ready(function(){
 
-  //MAIN DIVS
-    $(".cover").on("click", function(){
-      $(this).toggleClass("nicked");
-      $(".title").toggleClass("nicked");
+  let lazy = lazyload();
+      $("img.lazyload").lazyload();
+  });
 
+    $(".boring").on("click", function(){
       $(".infoButton").toggleClass("clicked");
       $(".info").toggleClass("clicked");
-
-    });
-
-    $(".title").on("click", function(){
-      $(this).toggleClass("nicked")
-      $(".cover").toggleClass("nicked")
-
-      $(".infoButton").toggleClass("clicked");
-      $(".info").toggleClass("clicked");
-
     });
 
     //slide toggle for project content
+    $(".collab").click(function(){
+        $(".content-collab").toggle();
+    });
+
+    $(".politicoin").click(function(){
+        $(".content-politicoin").toggle();
+    });
+
+    $(".muse").click(function(){
+        $(".content-muse").toggle();
+    });
 
     $(".evan-roth").click(function(){
-        // $(this).slideToggle()
         $(".content-evan-roth").toggle();
     });
 
+    $(".flare").click(function(){
+        $(".content-flare").toggle();
+    });
+
+    $(".shuffle").click(function(){
+        $(".content-shuffle").toggle();
+    });
 
     $(".epigenia").click(function(){
-        // $(this).slideToggle()
         $(".content-epigenia").toggle();
     });
 
+    $(".cscc").click(function(){
+        $(".content-cscc").toggle();
+    });
+
+    $(".bfc").click(function(){
+        $(".content-bfc").toggle();
+    });
+
     $(".streetlight").click(function(){
-        // $(this).slideToggle()
         $(".content-streetlight").toggle();
     });
 
     $(".edes").click(function(){
-        // $(this).slideToggle()
         $(".content-edes").toggle();
     });
 
     $(".manzanar").click(function(){
-        // $(this).slideToggle()
         $(".content-manzanar").toggle();
     });
 
     $(".raid").click(function(){
-        // $(this).slideToggle()
         $(".content-raid").toggle();
     });
 
     $(".posters").click(function(){
-        // $(this).slideToggle()
         $(".content-posters").toggle();
     });
 
     $(".cornell-dive").click(function(){
-        // $(this).slideToggle()
         $(".content-cornell-dive").toggle();
     });
 
-
-    
-let lazy = lazyload();
-
-    $("img.lazyload").lazyload();
-
-
-
-
-
-
+$('.owl-carousel').owlCarousel({
+    items:1,
+    lazyLoad:true,
+    margin:10,
+    autoplay:false,
+    // autoplayTimeout:5000,
+    loop:true
 });
-
-
-
-
-
-
