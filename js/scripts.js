c95406bf3d6061db105318126ad1e6a749c3f291
Mostyn Griffith (NewMAC) <>
2018-10-11 03:05:06 -0400

    wework v1

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,15 @@ $(document).ready(function(){
     img.onload = function() {
         img.removeAttribute('data-src');
     };
-});
+    });
+
+//     $('video').each(function(){
+//     if ($(this).is(":in-viewport")) {
+//         $(this)[0].play();
+//     } else {
+//         $(this)[0].pause();
+//     }
+// });
 
 });
 
